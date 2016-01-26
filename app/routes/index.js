3239d16f3d4daaa4f5f103faa2e22ad2c86839f1
pytong <pytong07@gmail.com>
2016-01-26 13:28:01 +0000

    Sync upload

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,15 @@
 "use strict";
 
-let path = process.cwd();
+let path = process.cwd(),
+	multer  = require("multer"),
+	upload = multer({ dest: "/tmp" });
 
 
 module.exports = (app) => {
+
+	app.post("/api/filemetadata", upload.single("the-file"), function (req, res, next) {
+		res.json({"fileSize": req.file.size});
+	});
 
 	app.get("/", (req, res)  => {
 		res.sendFile(path + "/public/index.html");
